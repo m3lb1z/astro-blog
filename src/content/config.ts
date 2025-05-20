88b589a1c85404014c9b640ff6968a7dc7d7da84
Elvis M. <u201112959@upc.edu.pe>
2025-05-20 07:32:33 -0500

    feat: add author profiles and update blog post authors to use references

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, reference } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
@@ -10,13 +10,24 @@ const blogCollection = defineCollection({
       image: image(),
 
       // Relation
-      author: z.string().optional(),
+      // author: z.string().optional(),
+      author: reference("author"),
 
       // Relation
       tags: z.array(z.string()).optional(),
     }),
 });
 
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
+});
+
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
